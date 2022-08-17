1b02b36016a22d6f7d3757e262a88d78fa72d747
jurip89 <juriparladore@MacBook-Pro-6.local>
2022-08-17 15:47:50 +0200

    just add the index.js file with a log

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('If you want to become a developer, you should do the Codaissieur academy. If you get stuck during the course you should google your problem and read the documentations!!! Good luck!')
